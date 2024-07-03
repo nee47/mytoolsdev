81c8d83f3635826f671d41ed28c78a4ba3905dfc
joseph <joseph_vv@hotmail.com>
2024-07-02 23:59:32 -0300

    shadcn installed

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/dark-mode-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-slate-900 flex-col items-center justify-between p-12">
-      <div className="text-white flex flex-col items-center">
+    <main className="flex min-h-screen  flex-col items-center justify-between p-12">
+      <div className=" flex flex-col items-center">
         <h1 className="text-6xl">SATOOLS</h1>
         <p className="text-xl">The simplest way to save your online tools</p>
+        <Button>CLICK ME</Button>
+        <ModeToggle></ModeToggle>
       </div>
     </main>
   );
