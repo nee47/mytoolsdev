cbdc73756ef216449cb3fae4a3f82bacdd4f775d
joseph <joseph_vv@hotmail.com>
2024-07-09 23:16:09 -0300

    n

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
 import AvatarButton from "@/components/avatar-button";
+import { cookies } from "next/headers";
 
-export default function Home() {
+async function getSessionData() {
+  const token = cookies().get("token")?.value;
+  console.log(`token: ${token}`);
+  return token;
+}
+
+export default async function Home() {
+  const tok = getSessionData();
   return (
     <main className="flex min-h-screen  flex-col items-center justify-between p-12">
       <div className=" flex flex-col items-center">
@@ -9,6 +17,8 @@ export default function Home() {
         <p className="text-xl">The simplest way to save your online tools</p>
         <AvatarButton></AvatarButton>
       </div>
+
+      <div></div>
     </main>
   );
 }
